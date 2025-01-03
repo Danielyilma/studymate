102728d1a52eb8e25981c6787e1d1f604b2349cc
daniel yilma <test@gmail.com>
2025-01-03 19:33:22 +0300

    adding a deployment script

---
+++ deploy.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# create and start a virtual environment
+python3 -m venv venv
+source venv/bin/activate
+
+pip install -r requirements.txt
+
+python3 manage.py migrate
+
+# deamon service configration
+project_service="[Unit]
+Description=Studymate Gunicorn Daemon
+After=network.target
+
+[Service]
+User=root
+Group=root
+WorkingDirectory=/home/ubuntu/studymate/
+ExecStart=/home/ubuntu/studymate/venv/bin/gunicorn --workers 3 --bind unix:/home/ubuntu/studymate/studymate.sock studymate.wsgi:application
+
+[Install]
+WantedBy=multi-user.target
+"
+
+echo "$project_service" | sudo tee /etc/systemd/system/gunicorn-studymate.service
+
+sudo systemctl daemon-reload
+
+sudo systemctl start gunicorn-studymate
+sudo systemctl enable gunicorn-studymate
+
+
+# nginx configration
+
+nginx_config="server {
+    listen 80;
+    server_name 54.160.76.121;
+
+    location / {
+        include proxy_params;
+        proxy_pass http://unix:/home/ubuntu/studymate/studymate.sock;
+    }
+}
+"
+
+echo "$nginx_config" | sudo tee /etc/nginx/sites-available/studymate
+
+sudo ln -s /etc/nginx/sites-available/studymate /etc/nginx/sites-enabled
+
+sudo nginx -t
+sudo systemctl restart nginx
